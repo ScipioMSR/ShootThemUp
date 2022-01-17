809ff6fbc1e674261aabb44249d3437aa4169bdb
Roman Brizinski <romanwt@mail.ru>
2022-01-18 00:26:09 +0300

    Init commit

---
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -0,0 +1,12 @@
+// Shoot Them Up. All rights reserved
+
+
+#include "STUGameModeBase.h"
+#include "Player/STUPlayerController.h"
+#include "Player/STUBaseCharacter.h"
+
+ASTUGameModeBase::ASTUGameModeBase()
+{
+	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+	PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
